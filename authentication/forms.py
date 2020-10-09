d3d3aa3171feb4720023b15ad92eb7c1622736c2
Luis Urán <luisuran.bit@gmail.com>
2020-10-09 01:59:00 -0300

    Traducción de la interfaz

--- authentication/forms.py
+++ authentication/forms.py
@@ -12,14 +12,14 @@ class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                "placeholder" : "Username",                
+                "placeholder" : "Nombre de usuario",                
                 "class": "form-control"
             }
         ))
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder" : "Password",                
+                "placeholder" : "Contraseña",                
                 "class": "form-control"
             }
         ))
@@ -28,28 +28,28 @@ class SignUpForm(UserCreationForm):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                "placeholder" : "Username",                
+                "placeholder" : "Nombre de usuario",                
                 "class": "form-control"
             }
         ))
     email = forms.EmailField(
         widget=forms.EmailInput(
             attrs={
-                "placeholder" : "Email",                
+                "placeholder" : "Dirección de correo electrónico",                
                 "class": "form-control"
             }
         ))
     password1 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder" : "Password",                
+                "placeholder" : "Contraseña",                
                 "class": "form-control"
             }
         ))
     password2 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder" : "Password check",                
+                "placeholder" : "Repetir contraseña",                
                 "class": "form-control"
             }
         ))
