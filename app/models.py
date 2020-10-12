85d56fd9c5d2bcb1afc64288852431745aad24b0
sanchezcristian <sanchezcristianm@gmail.com>
2020-10-11 21:02:57 -0300

    feature: added contact, rol, contact-rol models. added fixtures contact-rol initial

--- app/models.py
+++ app/models.py
@@ -5,5 +5,49 @@ class Usuario(models.Model):
     api_id = models.CharField(max_length=32)
     api_hash = models.CharField(max_length=32)
 
+class Contact(models.Model):
+    # Fields
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100, primary_key=True)
+    email = models.EmailField
+
+    # Metadata
+    class Meta:
+        ordering = ['first_name', 'last_name', 'name'] 
+
+    # Methods
+    def __str__(self):
+        return self.name
+
+class Rol(models.Model):
+    # Fields
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+
+    # Metadata
+    class Meta:
+        ordering = ['id', 'name'] 
+
+    # Methods
+    def __str__(self):
+        return self.name
+
+class ContactRol(models.Model):
+    # Fields
+    contact_name = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    rol_id = models.ForeignKey(Rol, on_delete=models.CASCADE)
+
 class Group(models.Model):
+    # Fields
     name = models.CharField(max_length=32)
+    contacts = models.ManyToManyField(Contact)
+
+    # Metadata
+    class Meta:
+        ordering = ['name'] 
+
+     # Methods
+    def __str__(self):
+        return self.name
