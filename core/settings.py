a4dba2dbd5a7f3c7c2f93e7999da636431c71289
sanchezcristian <sanchezcristianm@gmail.com>
2020-11-09 03:28:37 -0300

    feature: added messages to delete and edit events.

--- core/settings.py
+++ core/settings.py
@@ -8,6 +8,12 @@ import os
 from decouple import config
 from unipath import Path
 import dj_database_url
+from django.contrib.messages import constants as messages
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR    = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
